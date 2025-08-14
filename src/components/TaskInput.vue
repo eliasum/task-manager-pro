6451b14b5ca883ecb4213a10467a5aedab69256f
eliasum <eliasum@yandex.ru>
2025-08-14 17:32:44 +0700

    Создание базовой структуры Task Manager

---
+++ src/components/TaskInput.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const newTaskText = ref('')
+</script>
+
+<template>
+  <div>
+    <input v-model="newTaskText" placeholder="Что нужно сделать?" type="text" />
+    <button>Добавить</button>
+  </div>
+</template>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// В будущем задачи будут приходить сюда через props
+const tasks = [
+  { id: 1, text: 'Изучить основы Vue', completed: true },
+  { id: 2, text: 'Создать первые компоненты', completed: false },
+]
+</script>
+
+<template>
+  <ul>
+    <li v-for="task in tasks" :key="task.id">
+      {{ task.text }}
+    </li>
+  </ul>
+</template>
