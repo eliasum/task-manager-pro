171d86295e400817d86c163a3f7a3aba5e15c529
eliasum <eliasum@yandex.ru>
2025-08-26 19:21:10 +0700

    Изменение задач и добавление чекбоксов в TaskList.vue

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 // В будущем задачи будут приходить сюда через props
-const tasks = [
-  { id: 1, text: 'Изучить основы Vue', completed: true },
-  { id: 2, text: 'Создать первые компоненты', completed: false },
-]
+const tasks = ref([
+  { id: 1, text: 'Изучить методы массивов JS', completed: true },
+  { id: 2, text: 'Применить v-for в Vue', completed: false },
+  { id: 3, text: 'Выпить чаю', completed: false },
+])
 </script>
 
 <template>
   <ul>
     <li v-for="task in tasks" :key="task.id">
-      {{ task.text }}
+      <input type="checkbox" :checked="task.completed" />
+      <span>{{ task.text }}</span>
     </li>
   </ul>
 </template>
